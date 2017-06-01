1717646d44a82449b492efe088b931c30c85171e
Nick Schrader <nick.schrader@mailbox.org>
2017-06-01 21:21:44 +0200

    Fixed bluetooth button on emulator

--- app/src/main/java/davidfdez/capteuratmospherique/Settings.java
+++ app/src/main/java/davidfdez/capteuratmospherique/Settings.java
@@ -3,11 +3,8 @@ package davidfdez.capteuratmospherique;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,21 +12,12 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.os.Environment;
 
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.security.Timestamp;
 import java.util.ArrayList;
 import java.util.Set;
 
 public class Settings extends AppCompatActivity {
+    public static String EXTRA_ADDRESS = "device_address";
     Button btnPaired;
     Button btnExport;
     ListView devicelist;
@@ -37,80 +25,14 @@ public class Settings extends AppCompatActivity {
     //Bluetooth
     private BluetoothAdapter myBluetooth = null;
     private Set<BluetoothDevice> pairedDevices;
-    public static String EXTRA_ADDRESS = "device_address";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
-        Bundle bundle = getIntent().getExtras();
-        user = bundle.getString("user");
-
-        //Calling widgets
-        btnPaired = (Button)findViewById(R.id.button);
-        devicelist = (ListView)findViewById(R.id.listView);
-        btnExport = (Button)findViewById(R.id.expButton);
-
-
-        //if the device has bluetooth
-        myBluetooth = BluetoothAdapter.getDefaultAdapter();
-
-        if(myBluetooth == null)
-        {
-            //Show a mensag. that the device has no bluetooth adapter
-            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
-
-            //finish apk
-            finish();
-        }
-        else if(!myBluetooth.isEnabled())
-        {
-            //Ask to the user turn the bluetooth on
-            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(turnBTon,1);
-        }
-
-        btnPaired.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                pairedDevicesList();
-            }
-        });
-
-    }
-    private void pairedDevicesList()
-    {
-        pairedDevices = myBluetooth.getBondedDevices();
-        ArrayList list = new ArrayList();
-
-        if (pairedDevices.size()>0)
-        {
-            for(BluetoothDevice bt : pairedDevices)
-            {
-                list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
-            }
-        }
-        else
-        {
-            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
-        }
-
-        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
-        devicelist.setAdapter(adapter);
-        devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
-
-    }
-    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener()
-    {
-        public void onItemClick (AdapterView<?> av, View v, int arg2, long arg3)
-        {
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
             // Get the device MAC address, the last 17 chars in the View
             String info = ((TextView) v).getText().toString();
             String address = info.substring(info.length() - 17);
 
             // Make an intent to start next activity.
-            Intent serviceIntent = new Intent(Settings.this , BluetoothService.class);
+            Intent serviceIntent = new Intent(Settings.this, BluetoothService.class);
             serviceIntent.putExtra(EXTRA_ADDRESS, address);
             serviceIntent.putExtra("user", user);
 
@@ -120,60 +42,57 @@ public class Settings extends AppCompatActivity {
         }
     };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Bundle bundle = getIntent().getExtras();
+        user = bundle.getString("user");
+
+        //Calling widgets
+        btnPaired = (Button) findViewById(R.id.button);
+        devicelist = (ListView) findViewById(R.id.listView);
+        btnExport = (Button) findViewById(R.id.expButton);
+
+        //if the device has bluetooth
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+
+        btnPaired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (myBluetooth == null) {
+                    Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+                } else if (!myBluetooth.isEnabled()) {
+                    //Ask to the user turn the bluetooth on
+                    Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(turnBTon, 1);
+                } else
+                    pairedDevicesList();
+            }
+        });
+
+    }
+
+    private void pairedDevicesList() {
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice bt : pairedDevices) {
+                list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        devicelist.setAdapter(adapter);
+        devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
+
+    }
+
     public void enregistrer(View v) {
-        String nomarchivo = "DonneesCapteur";
-        String contenido = "";
-        try {
-            File tarjeta = Environment.getExternalStorageDirectory();
-            Toast.makeText(this,tarjeta.getAbsolutePath(),Toast.LENGTH_LONG).show();
-            File file = new File(tarjeta.getAbsolutePath(), nomarchivo);
-            OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(file)); ///ESTA LINEA FALLA NO SE POR QUE
-            AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,
-                    "administracion", null, 1);
-            SQLiteDatabase bd = admin.getWritableDatabase();
-            Cursor fila = bd.rawQuery(
-                    "select * from Mesure", null);
-            if (fila.moveToFirst()) {
-                contenido += fila.getString(0) + ";" + fila.getString(1) + ";" + fila.getInt(2) + ";" + fila.getInt(3) + ";" + fila.getString(4) + ";" + fila.getString(5) + ";" + fila.getDouble(6) + ";" + fila.getDouble(7) + ";" + fila.getInt(8) + ";" + fila.getString(9) + "\n";
-                while (fila.moveToNext()) {
-                    contenido += fila.getString(0) + ";" + fila.getString(1) + ";" + fila.getInt(2) + ";" + fila.getInt(3) + ";" + fila.getString(4) + ";" + fila.getString(5) + ";" + fila.getDouble(6) + ";" + fila.getDouble(7) + ";" + fila.getInt(8) + ";" + fila.getString(9) + "\n";
-                }
-                osw.write(contenido);
-                osw.flush();
-                osw.close();
-                Toast.makeText(this, "Los datos fueron grabados correctamente",
-                        Toast.LENGTH_SHORT).show();
-            }
-
-        } catch (IOException ioe) {
-            Toast.makeText(this, "No se pudo grabar",
-                    Toast.LENGTH_SHORT).show();
-        }
     }
-/*
-    public void recuperar(View v) {
-        String nomarchivo = et1.getText().toString();
-        File tarjeta = Environment.getExternalStorageDirectory();
-        File file = new File(tarjeta.getAbsolutePath(), nomarchivo);
-        try {
-            FileInputStream fIn = new FileInputStream(file);
-            InputStreamReader archivo = new InputStreamReader(fIn);
-            BufferedReader br = new BufferedReader(archivo);
-            String linea = br.readLine();
-            String todo = "";
-            while (linea != null) {
-                todo = todo + linea + " ";
-                linea = br.readLine();
-            }
-            br.close();
-            archivo.close();
-            et2.setText(todo);
-
-        } catch (IOException e) {
-            Toast.makeText(this, "No se pudo leer",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
-}*/
 
 }
